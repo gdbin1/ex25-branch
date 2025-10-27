6dd66bd001902fa9cd05ba6540ecd621484c1dfe
gdbin1 <ot5037@naver.com>
2025-10-27 15:15:18 +0900

    Update DBConneTest.java

--- src/main/java/com/gbk/mybatis/controller/DBConneTest.java
+++ src/main/java/com/gbk/mybatis/controller/DBConneTest.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DBConneTest {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(DBConneTest.class);
+	
 	@Autowired
 	private DataSource dataSource;
 	
@@ -20,6 +24,7 @@ public class DBConneTest {
 	public String dbconn() {
 		try (Connection conn = dataSource.getConnection()) {
 			String result = conn.getMetaData().getURL();
+			log.info(result);
 			return "DB연결 성공: " + result;
 		} catch (Exception e) {
 			e.printStackTrace();
