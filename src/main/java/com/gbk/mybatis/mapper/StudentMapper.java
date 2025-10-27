8a1041668767ce8d7448797373f54d24e2f72361
gdbin1 <ot5037@naver.com>
2025-10-27 15:55:25 +0900

    dsadsa

--- src/main/java/com/gbk/mybatis/mapper/StudentMapper.java
+++ src/main/java/com/gbk/mybatis/mapper/StudentMapper.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 //import org.apache.ibatis.annotations.Options;
 //import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Options;
 
 import com.gbk.mybatis.domain.Student;
 
@@ -15,15 +16,9 @@ public interface StudentMapper {
 	List<Student> findAll();
 
 	Student findById(Long id);
-//	@Select("SELECT * FROM student WHERE id = #{id}")
-//	Student findById(Long id);
 	
-	
+	@Options(useGeneratedKeys = true, keyProperty = "id")
 	void insert(Student student);
-//	@Insert("INSERT INTO student(NAME, email, age) \r\n"
-//			+ " 	VALUES (#{name}, #{email}, #{age})")
-//	@Options(useGeneratedKeys = true, keyProperty = "id")
-//	void insert(Student student);
 
 	void update(Student student);
 
