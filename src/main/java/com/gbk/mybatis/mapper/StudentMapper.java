446a93bc2c50f757892a5612f6ebe3ae8ed4590a
gdbin1 <ot5037@naver.com>
2025-10-28 10:29:48 +0900

    10-28_f2/mappersql

--- src/main/java/com/gbk/mybatis/mapper/StudentMapper.java
+++ src/main/java/com/gbk/mybatis/mapper/StudentMapper.java
@@ -2,10 +2,14 @@ package com.gbk.mybatis.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 //import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 //import org.apache.ibatis.annotations.Options;
 //import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.gbk.mybatis.domain.Student;
 
@@ -15,10 +19,21 @@ public interface StudentMapper {
 	List<Student> findAll();
 
 	Student findById(Long id);
+	@Select("INSERT INTO student(NAME, email, age) \r\n"
+			+ " VALUES (#{name}, #{email}, #{age})")
+	
 	
 	void insert(Student student);
-
+	@Insert("INSERT INTO student(NAME, email, age) \r\n"
+			+ "  	VALUES (#{name}, #{email}, #{age})")
+	
+	@Options(useGeneratedKeys = true, keyProperty = "id")
+	
 	void update(Student student);
-
+	@Update("UPDATE student\r\n"
+	+ "	SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
+	+ "	WHERE id = #{id}")
+	
 	void delete(Long id);
+//	@Delete("DELETE FROM student WHERE id = #{id}")
 }
