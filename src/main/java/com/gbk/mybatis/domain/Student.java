2119a895639e6ecef1890f27c0e2b7ef86b5dc5d
gdbin1 <ot5037@naver.com>
2025-10-28 16:26:52 +0900

    Squashed commit of the following:
    commit 337f8e5930d44e83fe4958c1ecac6797e877dfe7
    Author: gdbin1 <ot5037@naver.com>
    Date:   Tue Oct 28 10:11:24 2025 +0900
    
        10-28_f2/dbconn
    
    commit a2dfc6e28748c13601a6c682f7fac076d4d9b6f3
    Merge: 14f44c2 cf31c4a
    Author: gdbin1 <ot5037@naver.com>
    Date:   Tue Oct 28 09:36:20 2025 +0900
    
        Merge pull request #1 from gdbin1/develop
    
        Develop
    
    commit cf31c4a68aa9dd5d5d118c7136e74d1a449c56e3
    Author: gdbin1 <ot5037@naver.com>
    Date:   Mon Oct 27 15:20:21 2025 +0900
    
        develop
    
    commit 59887a85fda00d056528d662ae03c5ae0961b1f8
    Author: gdbin1 <ot5037@naver.com>
    Date:   Mon Oct 27 12:44:47 2025 +0900
    
        Update Ex25BranchApplication.java
    
    commit e8c5aec3bd22730abe171be12533e87712ca416d
    Author: gdbin1 <ot5037@naver.com>
    Date:   Mon Oct 27 12:00:44 2025 +0900
    
        10-27

---
+++ src/main/java/com/gbk/mybatis/domain/Student.java
@@ -0,0 +1,16 @@
+package com.gbk.mybatis.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+	private Long id;
+	private String name;
+	private String email;
+	private Integer age;
+	private LocalDateTime created_at;
+	private LocalDateTime updated_at;
+}
